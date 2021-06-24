23fe5b554e10067b046fae0f292a9383e1cd0c43
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2021-06-24 09:46:29 +0100

    2d array hourglass practice soltuions

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -13,13 +13,34 @@ class LinkedList:
 
         if self.head:
             current = self.head
-            while current.next:
+            while current.next != None:
                 current = current.next
 
             current.next = new_node
         else:
             self.head = new_node
 
+    # Returns the length (integer) of the linked list.
+    def length(self):
+        current = self.head
+        total = 0
+        while current.next != None:
+            total += 1
+            current = current.next
+        return total
+
+    # Prints out the linked list in traditional Python list format. 
+    def display(self):
+        nodes = []
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+            nodes.append(current_node.data)
+        
+        print(nodes)
+
+
+    #def get(self, index):
 
 
 
@@ -31,6 +52,7 @@ class LinkedList:
 
 a = LinkedListNode(3)
 my_ll = LinkedList(a)
+print(my_ll.display())
 my_ll.append(4)
 my_ll.append(5)
 my_ll.append(6)
@@ -38,4 +60,6 @@ print(my_ll.head.data) #3
 print(my_ll.head.next.data) #4
 print(my_ll.head.next.next.data) #5
 print(my_ll.head.next.next.next.data) #6
-print(my_ll.head.next.next.next.next.data) #NoneType
+print(my_ll.length())
+print(my_ll.display())
+#print(my_ll.head.next.next.next.next.data) #NoneType
